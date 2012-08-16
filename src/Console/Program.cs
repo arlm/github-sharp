c0edceaea0a4f400d591a1ac013f3620eeaa9533
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2012-08-16 19:20:46 -0300

    * Get user repositories

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using GitHubSharp;
 using GitHubSharp.JSON;
+using ServiceStack.Text;
 
 namespace ConsoleClient
 {
@@ -14,8 +15,18 @@ namespace ConsoleClient
             GitHub github = new GitHub("username", "password");
 
             Rate rate = github.Limit;
-
+            
             Console.Out.WriteLine("Limit {0} of {1}", rate.Remaining, rate.Limit);
+
+            List<Repo> repos = github.Repos;
+            Console.Out.WriteLine("{0} Repos:", repos.Count);
+
+            foreach (Repo repo in repos)
+            {
+                Console.Out.Write("{0},", repo.Name);
+            }
+
+            Console.Out.WriteLine("\n\nRepo {0}:\n{1}", repos[0].Name, repos[0].Dump());
 
             Console.In.Read();
         }
